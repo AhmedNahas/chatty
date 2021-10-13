ad69122ff3c9f1f9b6fe3d274e1cc89c8f731a50
ahmedNahas <ahmed_elnahas@live.com>
2021-10-14 01:36:40 +0200

    fix create post issue (render flex)

--- lib/screens/new_post/new_post.dart
+++ lib/screens/new_post/new_post.dart
@@ -36,89 +36,91 @@ class NewPost extends StatelessWidget {
                       },
                       child: const Text('Post')),
                 ]),
-            body: userModel != null ? Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  if (state is CreatePostLoadingState)
-                    const LinearProgressIndicator(
-                      minHeight: 10.0,
+            body: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: userModel != null ? Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    if (state is CreatePostLoadingState)
+                      const LinearProgressIndicator(
+                        minHeight: 10.0,
+                      ),
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 25.0,
+                          backgroundImage: NetworkImage(userModel!.image),
+                        ),
+                        const SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(userModel!.name,
+                            style: Theme.of(context).textTheme.subtitle1!),
+                      ],
                     ),
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 25.0,
-                        backgroundImage: NetworkImage(userModel!.image),
-                      ),
-                      const SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(userModel!.name,
-                          style: Theme.of(context).textTheme.subtitle1!),
-                    ],
-                  ),
-                  Expanded(
-                    child: TextFormField(
+                    TextFormField(
                       controller: postController,
                       decoration: const InputDecoration(
                         hintText: "What's on your mind ...",
                         border: InputBorder.none,
                       ),
+                      maxLines: 10,
                     ),
-                  ),
-                  if (cubit.postImageFile != null)
-                    Stack(
-                      alignment: AlignmentDirectional.topEnd,
-                      children: [
-                        Stack(
-                          alignment: AlignmentDirectional.topEnd,
-                          children: [
-                            Align(
-                              alignment: AlignmentDirectional.topCenter,
-                              child: Container(
-                                height: 180.0,
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: FileImage(cubit.postImageFile!),
+                    if (cubit.postImageFile != null)
+                      Stack(
+                        alignment: AlignmentDirectional.topEnd,
+                        children: [
+                          Stack(
+                            alignment: AlignmentDirectional.topEnd,
+                            children: [
+                              Align(
+                                alignment: AlignmentDirectional.topCenter,
+                                child: Container(
+                                  height: 180.0,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: FileImage(cubit.postImageFile!),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: CircleAvatar(
-                                  child: IconButton(
-                                    icon: const Icon(Icons.close),
-                                    onPressed: () {
-                                      cubit.removePostImage();
-                                    },
-                                  )),
-                            )
-                          ],
-                        ),
-                      ],
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: CircleAvatar(
+                                    child: IconButton(
+                                      icon: const Icon(Icons.close),
+                                      onPressed: () {
+                                        cubit.removePostImage();
+                                      },
+                                    )),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    TextButton(
+                      onPressed: () {
+                        cubit.getPostImage();
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.image_outlined),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text("Add photo"),
+                        ],
+                      ),
                     ),
-                  TextButton(
-                    onPressed: () {
-                      cubit.getPostImage();
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(Icons.image_outlined),
-                        SizedBox(
-                          width: 5.0,
-                        ),
-                        Text("Add photo"),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ) : const Center(child: CircularProgressIndicator()),
+                  ],
+                ),
+              ) : const Center(child: CircularProgressIndicator()),
+            ),
           );
         });
   }
